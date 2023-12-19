9161c8baba78964c5e92e7ad0fe87176341e0963
Stefanija Duracoska <sduracoska@gmail.com>
2023-12-19 21:01:40 +0100

    change open api port

--- apps/api/src/openapi-spec.ts
+++ apps/api/src/openapi-spec.ts
@@ -7,7 +7,7 @@ import { ApiApplication } from './application'
 async function exportOpenApiSpec(): Promise<void> {
   const config: ApplicationConfig = {
     rest: {
-      port: +(process.env.PORT ?? 3000),
+      port: +(5001),
       host: process.env.HOST ?? 'localhost',
     },
   }
