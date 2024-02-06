a70597973f1d205292e2dddb48c2a5c9fc20d87e
Stefanija Duracoska <sduracoska@gmail.com>
2024-02-06 18:13:29 +0100

    Add namespace

--- apps/api/src/application.ts
+++ apps/api/src/application.ts
@@ -1,56 +1,56 @@
-import { BootMixin } from '@loopback/boot'
-import { ApplicationConfig } from '@loopback/core'
+import { BootMixin } from "@loopback/boot";
+import { ApplicationConfig } from "@loopback/core";
 import {
   RestExplorerBindings,
   RestExplorerComponent,
-} from '@loopback/rest-explorer'
-import { RepositoryMixin } from '@loopback/repository'
-import { RestApplication } from '@loopback/rest'
-import { ServiceMixin } from '@loopback/service-proxy'
-import path from 'path'
-import { MySequence } from './sequence'
-import { CHAT_SERVICE, USERS_SERVICE } from './domains/users/keys'
-import { UserService } from './domains/users/services/user.service'
-import { ChatService } from './domains/chat/services/chat.service'
+} from "@loopback/rest-explorer";
+import { RepositoryMixin } from "@loopback/repository";
+import { RestApplication } from "@loopback/rest";
+import { ServiceMixin } from "@loopback/service-proxy";
+import path from "path";
+import { MySequence } from "./sequence";
+import { CHAT_SERVICE, USERS_SERVICE } from "./domains/users/keys";
+import { UserService } from "./domains/users/services/user.service";
+import { ChatService } from "./domains/chat/services/chat.service";
 
-export { ApplicationConfig }
+export { ApplicationConfig };
 
 export class ApiApplication extends BootMixin(
-  ServiceMixin(RepositoryMixin(RestApplication)),
+  ServiceMixin(RepositoryMixin(RestApplication))
 ) {
   constructor(options: ApplicationConfig = {}) {
-    super(options)
+    super(options);
 
-    this.setupBindings()
+    this.setupBindings();
 
-    this.sequence(MySequence)
+    this.sequence(MySequence);
 
-    this.static('/', path.join(__dirname, '../public'))
+    this.static("/", path.join(__dirname, "../public"));
 
     this.configure(RestExplorerBindings.COMPONENT).to({
-      path: '/explorer',
-    })
-    this.component(RestExplorerComponent)
+      path: "/explorer",
+    });
+    this.component(RestExplorerComponent);
 
-    this.projectRoot = __dirname
+    this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
       controllers: {
         // Customize ControllerBooter Conventions here
-        dirs: ['domains/**/**/controllers'],
-        extensions: ['.controller.js'],
+        dirs: ["domains/**/**/controllers"],
+        extensions: [".controller.js"],
         nested: true,
       },
       repositories: {
-        dirs: ['domains/**/repositories'],
-        extensions: ['.repository.js'],
+        dirs: ["domains/**/repositories"],
+        extensions: [".repository.js"],
         nested: true,
       },
-    }
+    };
   }
 
   setupBindings(): void {
-    this.bind(USERS_SERVICE).toClass(UserService)
-    this.bind(CHAT_SERVICE).toClass(ChatService)
+    this.bind(USERS_SERVICE).toClass(UserService);
+    this.bind(CHAT_SERVICE).toClass(ChatService);
   }
 }
